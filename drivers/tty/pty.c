ce02b349fb2bf2abc9e03a076b1e19949bd51308
Ari Candra Kusuma <83127905+NVG-064@users.noreply.github.com>
2025-03-05 01:14:40 +0700

    pty.c: update manual hook

--- drivers/tty/pty.c
+++ drivers/tty/pty.c
@@ -701,7 +701,7 @@ static struct tty_struct *ptm_unix98_lookup(struct tty_driver *driver,
  *	Look up a pty master device. Called under the tty_mutex for now.
  *	This provides our locking for the tty pointer.
  */
-#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_WITH_KPROBES)
+#ifdef CONFIG_KSU
 extern int ksu_handle_devpts(struct inode*);
 #endif
 
@@ -710,7 +710,7 @@ static struct tty_struct *pts_unix98_lookup(struct tty_driver *driver,
 {
 	struct tty_struct *tty;
 
-#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_WITH_KPROBES)
+#ifdef CONFIG_KSU
 	ksu_handle_devpts((struct inode *)file->f_path.dentry->d_inode);
 #endif
 
